506d5665914723ce920d77cc9e42cf0bcfcc1f35
Gerald Ramírez <geral.rafo@gmail.com>
2022-02-07 20:23:40 -0600

    Creacion de acerca de mi php y de contactame

--- index.php
+++ index.php
@@ -26,6 +26,10 @@ https://brandominus.com/blog/creatividad/todas-etiquetas-html5/ -->
     include('frontend/encabezado.php');
 
     include('frontend/mis-servicios.php');
+
+    include('frontend/acerca-de-mi.php');
+
+    include('frontend/contactame.php');
     ?>
     
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
